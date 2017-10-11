e117cc649ce0052f44b7d462722ff1179c2b8ca8
Kitsu <mail@kitsu.me>
2017-10-12 01:05:31 +0300

    Fixes scripts
    - fixes bugs
    - more clear and commented code
    - consistent naming
    - docker-machine-env instead of ssh-ing

--- scripts/build-local-image.sh
+++ scripts/build-local-image.sh
@@ -1,23 +1,24 @@
 #!/bin/sh -x
 
 
-IROHA_HOME=$(dirname $(realpath ${BASH_SOURCE[0]}))/..
+IROHA_HOME=$(dirname $(realpath $0))/..
 MOUNT=/tmp/iroha
 BUILD_PATH=/tmp/build
 PROJ_BIN=/opt/iroha
-docker pull hyperledger/iroha-docker-develop
-# docker build $IROHA_HOME/docker/develop -t iroha-dev
-echo $IROHA_HOME\;$MOUNT\;$BUILD_PATH\;$PROJ_BIN
-# docker run -tdv $IROHA_HOME:$MOUNT hyperledger/iroha-docker-develop "/bin/bash" >/tmp/iroha-build.id
-CONTAINER=$(cat /tmp/iroha-build.id)
-docker exec $CONTAINER sh <<EOF
+
+# You can either pull image or build your own (this is only base for iroha)
+# docker pull hyperledger/iroha-docker-develop
+docker build $IROHA_HOME/docker/develop -t hyperledger/iroha-docker
+
+# The actual build
+docker run -tdv $IROHA_HOME:$MOUNT hyperledger/iroha-docker sh <<EOF
 mkdir -p $BUILD_PATH $PROJ_BIN;
 cd $BUILD_PATH && cmake -DTESTING=OFF $MOUNT && make;
 cp $BUILD_PATH/bin/* $PROJ_BIN;
-ldd $PROJ_BIN/* | egrep -o '\/.*?\.(so(\..*?)?|a)\s' | grep $MOUNT | uniq | xargs cp -t $PROJ_BIN | true;
+ldd $PROJ_BIN/* | egrep '(ed25519|gflags|boost)' | sort | uniq | egrep -o '\/.*?\.(so(\..*?)?|a)\s' | xargs cp -t /usr/lib;
+ldd $PROJ_BIN/* | grep stdc++ | head -n 1 | egrep -o '\/.*?\.(so(\..*?)?|a)\s' | xargs cp -t /usr/lib/x86_64-linux-gnu/
 EOF
 
-docker commit $CONTAINER iroha-dev
-docker stop $CONTAINER && docker rm $CONTAINER;
+docker commit $CONTAINER hyperledger/iroha-docker
+docker stop $CONTAINER && docker rm $CONTAINER
 docker save -o "$IROHA_HOME"/build/iroha-dev.tar hyperledger/iroha-docker
-
--- scripts/build-test-swarm.sh
+++ scripts/build-test-swarm.sh
@@ -4,41 +4,43 @@ if [ -z "$1" ] || [ "$1" -le "0" ]; then
     echo "Usage: $0 <number_of_peers>"
     exit -1
 fi
-PEERS_NUM=$(($1-1))
-
-
-if ! docker ps >/dev/null 2>&1; then
-    echo "Dockerd not running, fix that please"
-    exit 2
-fi
+LAST_PEER_ID=$(($1-1))
 
 if ! docker-machine >/dev/null 2>&1; then
     echo "docker-machine not found"
     exit 3
 fi
 
-IROHA_HOME=$(dirname $0)/..
+IROHA_HOME=$(dirname $0)/../
 IMAGE="$IROHA_HOME/build/iroha-dev.tar"
 PREFIX=peer
 
-for i in $(seq 0 $PEERS_NUM); do
+# Create nodes
+for i in $(seq 0 $LAST_PEER_ID); do
     docker-machine create "$PREFIX$i";
-    
-    if [ -f "$IMAGE" ]; then
-        docker-machine ssh "$PREFIX$i" "docker load -i $IMAGE";
-    fi
-    
-    if [ "$i" -eq 0 ]; then
-        INVITE=$(docker-machine ssh ${PREFIX}0 'docker swarm init --advertise-addr=$(docker-machine) env '${PREFIX}0'
-                    | egrep -o "[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")' \
-                | egrep -o 'docker\sswarm\sjoin\s.*$')
-    else
-        docker-machine ssh "$PREFIX$i" "$INVITE";
-        # only for tests, consider ~3 manager nodes in production
-        docker-machine ssh ${PREFIX}0 "docker node promote $PREFIX$i";
-    fi
-
 done
 
-$(dirname $(realpath $0))/swarm-deploy.sh
+# Manager node
+IP=$(docker-machine inspect ${PREFIX}0 | grep IPAddress | egrep -o "[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")
+eval $(docker-machine env "${PREFIX}0")
+INVITE=$(docker swarm init --advertise-addr=$IP | egrep -o 'docker\sswarm\sjoin\s.*$')
+
+# Join to the swarm by the rest
+for i in $(seq 1 $LAST_PEER_ID); do
+    eval $(docker-machine env "$PREFIX$i")
+    eval $INVITE
+
+    # only for tests
+    docker-machine ssh "${PREFIX}0" node promote "$PREFIX$i";
+done
+
+# Load local image if possible
+if [ -f "$IMAGE" ]; then
+    for i in $(seq 0 $LAST_PEER_ID); do
+        eval $(docker-machine env "$PREFIX$i")
+        docker load -i $IMAGE;
+    done
+fi
+
+eval $(docker-machine env -u)
 
