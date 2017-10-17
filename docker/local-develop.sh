5cede7d6966e5a804e76a806107332806532d7c6
Bogdan Vaneev <warchantua@gmail.com>
2017-10-17 19:44:45 +0300

    Fix tests
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- docker/local-develop.sh
+++ docker/local-develop.sh
@@ -10,28 +10,43 @@ dependency grep   || exit 3
 dependency sed    || exit 4
 
 # usage
-echo "execute 'source $0' to get all env vars for testing"
+# $ 
 
 # cleanup
-docker rm -f pg rd 2>/dev/null
+docker rm -f pg rd 2>/dev/null 1>&2
 
 # vars
-user=iroha
-password=iroha
+user=postgres
+password=mysecretpassword
+pgport=5432
+rdport=6379
 
 # run postgres and redis
-pgid=$(docker run --rm --name pg -e POSTGRES_USER=$user -e POSTGRES_PASSSWORD=$password -d postgres:9.5)
-rdid=$(docker run --rm --name rd -d redis:3.2.8)
+pgid=$(docker run -p 127.0.0.1:$pgport:$pgport --rm --name pg -e POSTGRES_USER=$user -e POSTGRES_PASSSWORD=$password -d postgres:9.5)
+rdid=$(docker run -p 127.0.0.1:$rdport:$rdport --rm --name rd -d redis:3.2.8)
 
 pgip=$(docker inspect $pgid | jq .[0].NetworkSettings.IPAddress | sed "s/\"//g")
 rdip=$(docker inspect $rdid | jq .[0].NetworkSettings.IPAddress | sed "s/\"//g")
 
-echo "postgres: $pgip:5432 {login: $user, pwd: $password}"
-echo "redis:    $rdip:6379"
+# stderr
+>&2 echo "postgres: $pgip:$pgport {login: $user, pwd: $password}"
+>&2 echo "redis:    $rdip:$rdport"
 
+# print variables (stdout)
+cat << EOF
 export IROHA_POSTGRES_HOST=$pgip
-export IROHA_POSTGRES_PORT=5432
+export IROHA_POSTGRES_PORT=$pgport
 export IROHA_POSTGRES_USER=$user
 export IROHA_POSTGRES_PASSWORD=$password
-export IROHA_REDIS_HOST=$rgip
-export IROHA_REDIS_PORT=6379
+export IROHA_REDIS_HOST=$rdip
+export IROHA_REDIS_PORT=$rdport
+EOF
+
+# apply variables
+export IROHA_POSTGRES_HOST=$pgip
+export IROHA_POSTGRES_PORT=$pgport
+export IROHA_POSTGRES_USER=$user
+export IROHA_POSTGRES_PASSWORD=$password
+export IROHA_REDIS_HOST=$rdip
+export IROHA_REDIS_PORT=$rdport
+
