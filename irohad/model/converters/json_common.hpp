772249ef92871777358d82bae4dfcdf7f98d795d
motxx <motohiko.ave@gmail.com>
2017-10-24 01:33:20 +0900

    Add Pager JSON convertor
    Signed-off-by: motxx <motohiko.ave@gmail.com>

--- irohad/model/converters/json_common.hpp
+++ irohad/model/converters/json_common.hpp
@@ -226,6 +226,17 @@ namespace iroha {
       }
 
       template <>
+      struct Convert<Pager> {
+        template <typename T>
+        auto operator()(T &&x) {
+          auto des = makeFieldDeserializer(x);
+          return nonstd::make_optional<Pager>()
+                 | des.String(&Pager::tx_hash, "tx_hash")
+                 | des.Uint(&Pager::limit, "limit");
+        }
+      };
+
+      template <>
       struct Convert<Signature> {
         template <typename T>
         auto operator()(T &&x) {
@@ -253,6 +264,15 @@ namespace iroha {
                                  acc_signatures);
         }
       };
+
+      /**
+       * Serialize pager to JSON with given allocator
+       * @param pager - pager for restricting to get transactions
+       * @param allocator - allocator for JSON value
+       * @return JSON value with pager
+       */
+      rapidjson::Value serializePager(
+        const Pager &pager, rapidjson::Document::AllocatorType &allocator);
 
       /**
        * Serialize signature to JSON with given allocator
